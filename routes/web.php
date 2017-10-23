c63113af45638d09fbe67d84083f5806826f711d
soniaodea <soniaodea@gmail.com>
2017-10-23 17:03:32 +0200

    Multiauthenticatión with Laravel

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 //Route::post('/user/logout', 'Auth\LoginController@userLogout')->name('user.logout');
 
 
