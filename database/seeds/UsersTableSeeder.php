6570201e1a428784636808a3f3137bba4a03f21a
soniaodea <soniaodea@gmail.com>
2017-10-23 17:00:40 +0200

    Initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            'name' => 'User',
+            'email' => 'user@mail.com',
+            'password' => bcrypt('password')
+        ];
+
+        $admin = [
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password')
+        ];
+        DB::table('users')->insert($user);
+        DB::table('admins')->insert($admin);
+    }
+}
---
+++ resources/views/proba.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Soniaren proba</title>
+</head>
+<body>
+KKDELAVACA
+</body>
+</html>
